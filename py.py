a39c828cb0ca1993854cfc6ff7c5e35223400503
Neo0732 <73626332+Neo0732@users.noreply.github.com>
2024-12-11 15:23:41 +0900

    241211_1523 주석 한글화/주석 보강

--- py.py
+++ py.py
@@ -1,18 +1,19 @@
 
 
-origin=(input("변환할 정수를 입력하십시오")) # USER_INPUT_VAL
-originFlag=(int(input("어떤 진수인지 입력하여 주십시오 (예, 10진수->10)"))) # ORIGIN_VAL_EVAL
-cnvrt=(int(input("어떤 진수로 변환할지 입력하십시오 (예, 2진법->2)"))) # origin TO
-fin='' # CONVERTED_VAL
+origin=(input("변환할 정수를 입력하십시오")) # 사용자가 원본 정수 입력
+originFlag=(int(input("어떤 진수인지 입력하여 주십시오 (예, 10진수->10)"))) # 사용자가 출발 진수 입력
+cnvrt=(int(input("어떤 진수로 변환할지 입력하십시오 (예, 2진법->2)"))) # 사용자가 도착 진수 입력
+fin='' # 변환된 진수 저장소
 
 if originFlag==10: # 출발 진수가 10진수
-    if cnvrt==2: #2진수
+    if cnvrt==2: #2진수 변환
        fin=(bin(int(origin))[2:])
-    elif cnvrt==8: #8진수
+    elif cnvrt==8: #8진수 변환
        fin=(oct(int(origin))[2:])
-    elif cnvrt==10: #10진수
-       print("And Don't you know how it sweet it tastes?")
-    elif cnvrt==12: #12진수
+    elif cnvrt==10: #10진수 변환
+       print("And Don't you know how it sweet it tastes?") # NewJeans - How Sweet
+       fin=origin
+    elif cnvrt==12: #12진수 변환
          def duodemic(q, w):
              revBase=''
              while q>0:
@@ -20,9 +21,9 @@ if originFlag==10: # 출발 진수가 10진수
               revBase+=str(mod)
               return revBase[::-1]
          fin=(duodemic(int(origin), 12))
-    elif cnvrt==16: #16진수
+    elif cnvrt==16: #16진수 변환
         fin=(hex(int(origin))[2:])
-    elif cnvrt==60: #60진법
+    elif cnvrt==60: #60진법 변환
         def sexage(q, w):
              revBase=''
              while q>0:
