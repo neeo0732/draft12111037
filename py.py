1987d4b85bf38e4ff088e7da3c29ea08b48a7d7f
neeo0732 <neo0732@anbeoph.goe.go.kr>
2024-12-12 11:23:39 +0900

    241212_1122 2진수완료+8진수완료

--- py.py
+++ py.py
@@ -35,39 +35,51 @@ if originFlag==10: # 출발 진수가 10진수
         print("지원하지 않는 진법이거나 내부 오류가 발생했습니다.")
 
 if originFlag==2: # 출발 진수가 2진수
-    def begi(q, w):
+    def BinarySet(q, w):
              revBase=''
              while q>0:
               q,mod=divmod(q, w)
               revBase+=str(mod)
               return revBase[::-1]
-             fin=(sexage(int(origin), 60))
+             fin=(BinarySet(int(origin), 60))
     if cnvrt==2: # 2진수 변환
        print("처음으로 나는 죽음을 맛보았다. 죽음은 쓴맛이었다. 왜냐하면 그것은 탄생이니까, 두려운 새 삶에 대한 불안과 걱정이니까.") # 데미안 - 헤르만 헤세
        fin=origin
     elif cnvrt==8: # 8진수 변환
-       fin=(oct(int(origin))[2:])
+       fin=(BinarySet(int(origin,2),8))
     elif cnvrt==10: # 10진수 변환
-       print("And Don't you know how it sweet it tastes?") # NewJeans - How Sweet
-       fin=origin
+       fin=int(origin,2)
     elif cnvrt==12: # 12진수 변환
-         def duodemic(q, w):
-             revBase=''
-             while q>0:
-              q,mod=divmod(q, w)
-              revBase+=str(mod)
-              return revBase[::-1]
-         fin=(duodemic(int(origin), 12))
+        fin=(BinarySet(int(origin,2),12))
     elif cnvrt==16: # 16진수 변환
-        fin=(hex(int(origin))[2:])
+        fin=(BinarySet(int(origin,2),16))
     elif cnvrt==60: # 60진법 변환
-        def sexage(q, w):
+        fin=(BinarySet(int(origin,2),60))
+    else:
+        print("지원하지 않는 진법이거나 내부 오류가 발생했습니다.")
+
+
+if originFlag==8: # 출발 진수가 8진수
+    def OctaSet(q, w):
              revBase=''
              while q>0:
               q,mod=divmod(q, w)
               revBase+=str(mod)
               return revBase[::-1]
-             fin=(sexage(int(origin), 60))
+             fin=(OctaSet(int(origin), 60))
+    if cnvrt==2: # 2진수 변환
+       fin=(OctaSet(int(origin,8),2))
+    elif cnvrt==8: # 8진수 변환
+       print("lorem ipsum은, 디자인, 폰트와 같은 조형물을 테스트하는 데 사용되어지는 더미 텍스트입니다.") # Lorem Ipsum
+       fin=origin
+    elif cnvrt==10: # 10진수 변환
+       fin=(OctaSet(int(origin,8),10))
+    elif cnvrt==12: # 12진수 변환
+        fin=(OctaSet(int(origin,8),12))
+    elif cnvrt==16: # 16진수 변환
+        fin=(OctaSet(int(origin,8),16))
+    elif cnvrt==60: # 60진법 변환
+        fin=(OctaSet(int(origin,8),60))
     else:
         print("지원하지 않는 진법이거나 내부 오류가 발생했습니다.")
 
